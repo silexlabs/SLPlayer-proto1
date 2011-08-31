c6e31800213e555f851342c5b050ecabbfc3fcf0
yanhick <yannick.dominguez@gmail.com>
2011-08-31 10:33:47 +0200

    repackage test folder

--- test/bin/runtime/domObject/domObjectTestPHP.php/lib/org/silex/runtime/domobject/GradientTypeValue.enum.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class org_silex_runtime_domobject_GradientTypeValue extends Enum {
-	public static $linear;
-	public static $radial;
-	public static $__constructors = array(0 => 'linear', 1 => 'radial');
-	}
-org_silex_runtime_domobject_GradientTypeValue::$linear = new org_silex_runtime_domobject_GradientTypeValue("linear", 0);
-org_silex_runtime_domobject_GradientTypeValue::$radial = new org_silex_runtime_domobject_GradientTypeValue("radial", 1);
--- test/bin/runtime/domObject/domObjectTestPHP.php/lib/org/silex/runtime/domobject/TransformationOriginValue.enum.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class org_silex_runtime_domobject_TransformationOriginValue extends Enum {
-	public static function constant($transformationOriginX, $transformationOriginY) { return new org_silex_runtime_domobject_TransformationOriginValue("constant", 0, array($transformationOriginX, $transformationOriginY)); }
-	public static function point($point) { return new org_silex_runtime_domobject_TransformationOriginValue("point", 1, array($point)); }
-	public static $__constructors = array(0 => 'constant', 1 => 'point');
-	}
